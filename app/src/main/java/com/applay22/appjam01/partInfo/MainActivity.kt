52e1b9a6a42d0967aca09d99f34b83b7f15eb6c3
bbasung1 <bbasung1@daum.net>
2022-05-14 17:10:40 +0900

    intent01

--- app/src/main/java/com/applay22/appjam01/partInfo/MainActivity.kt
+++ app/src/main/java/com/applay22/appjam01/partInfo/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.applay22.appjam01.partInfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
 import com.applay22.appjam01.partInfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mactivityreslauncher:ActivityResultLauncher<Intent>
     private val binding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -12,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.applycheck.setOnClickListener{
-
+            val mIntent=Intent(this,dataManager::class.java)
+            intent.extras?.putFloat("test",binding.curkminput.toString().toFloat())
+            mactivityreslauncher.launch(mIntent)
         }
     }
 }
