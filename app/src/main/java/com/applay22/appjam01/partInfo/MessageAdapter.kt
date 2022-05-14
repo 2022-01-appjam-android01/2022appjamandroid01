436d5daef471169f3d5c22e2ce473bdcd1456c3a
bbasung1 <bbasung1@daum.net>
2022-05-14 23:21:04 +0900

    textwatchertest

--- app/src/main/java/com/applay22/appjam01/partInfo/MessageAdapter.kt
+++ app/src/main/java/com/applay22/appjam01/partInfo/MessageAdapter.kt
@@ -1,6 +1,8 @@
 package com.applay22.appjam01.partInfo
 
+import android.text.Editable
 import android.text.Layout
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -17,7 +19,7 @@ class MessageAdapter(private var data:Array<String>,private var dist:Array<Strin
 
     override fun onBindViewHolder(holder: MessageAdapter.MessageViewHolder, position: Int) {
         holder.listitem.text=data[position]
-        dist[position]=holder.test.toString()
+        val item=dist[position]
     }
 
     override fun getItemCount(): Int {
@@ -26,5 +28,22 @@ class MessageAdapter(private var data:Array<String>,private var dist:Array<Strin
     inner class MessageViewHolder(private val binding: ActivityListItemBinding) : RecyclerView.ViewHolder(binding.root){
         var listitem=binding.textView6
         var test=binding.recentkm
+        init{
+            test.addTextChangedListener(object : TextWatcher{
+                override fun afterTextChanged(p0: Editable?) {
+                    dist[adapterPosition]=p0.toString()
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+            })
+        }
     }
+}
+
 }
