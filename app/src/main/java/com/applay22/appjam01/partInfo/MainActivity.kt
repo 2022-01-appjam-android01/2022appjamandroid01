9fc6b76419f132147c16237ba690f787751a7776
bbasung1 <bbasung1@daum.net>
2022-05-14 19:33:16 +0900

    recyclerview

--- app/src/main/java/com/applay22/appjam01/partInfo/MainActivity.kt
+++ app/src/main/java/com/applay22/appjam01/partInfo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.applay22.appjam01.partInfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val data=arrayOf<String>("오일 필터","미션 오일","브레이크 오일","파워 오일","스파크 플러그","타이밍 벨트","브레이킹 패드","브레이크 라이닝","클러치 디스크","타이어 부분 변경","타이어","배터리","결빙 방지","에어컨 필터","와이퍼")
+        binding.rvView.adapter=MessageAdapter(data)
+        binding.rvView.layoutManager=LinearLayoutManager(this)
         binding.applycheck.setOnClickListener{
             val mIntent=Intent(this,dataManager::class.java)
             var bundle=Bundle()
--- app/src/main/java/com/applay22/appjam01/partInfo/MessageAdapter.kt
+++ app/src/main/java/com/applay22/appjam01/partInfo/MessageAdapter.kt
@@ -1,25 +1,28 @@
 package com.applay22.appjam01.partInfo
 
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.applay22.appjam01.partInfo.databinding.ActivityListItemBinding
 
-class MessageAdapter(private data:Array<String>) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>(){
+class MessageAdapter(private var data:Array<String>) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>(){
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MessageAdapter.MessageViewHolder {
-
+        val binding=ActivityListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MessageViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MessageAdapter.MessageViewHolder, position: Int) {
-
+        holder.listitem.text=data[position]
     }
 
     override fun getItemCount(): Int {
-
+        return data.size
     }
     inner class MessageViewHolder(private val binding: ActivityListItemBinding) : RecyclerView.ViewHolder(binding.root){
-
+        var listitem=binding.textView6
     }
 }
