de4fdf3db4c3826f92390e141b0c6c5fc0ad9767
Insiro <leeon@insiro.me>
2022-05-14 15:59:26 +0900

    add: PartField dataclass

---
+++ app/src/main/java/com/applay22/appjam01/partInfo/PertFields.kt
@@ -0,0 +1,22 @@
+package com.applay22.appjam01.partInfo
+
+@kotlinx.serialization.Serializable
+data class PertFields(
+    var oil_filter: Float = 0f,
+    var mission_oil: Float = 0f,
+    var break_oil: Float = 0f,
+    var power_oil: Float = 0f,
+    var spark_plugs: Float = 0f,
+    var timing_valt: Float = 0f,
+    var break_pad: Float = 0f,
+    var Brake_lining: Float = 0f,
+    var Clutch_Disc: Float = 0f,
+    var tier_location_change: Float = 0f,
+    var tier_change: Float = 0f,
+    var wile_alignments: Float = 0f,
+    var battery: Float = 0f,
+    var anti_freeze: Float = 0f,
+    var air_controller: Float = 0f,
+    var wiper: Float = 0f,
+
+    )
