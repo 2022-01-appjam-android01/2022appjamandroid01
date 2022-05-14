0bee30885a974fa1831762341767e03a30660ab8
bbasung1 <bbasung1@daum.net>
2022-05-14 15:56:52 +0900

    view binding

--- app/src/main/java/com/example/a2022appjam01team/MainActivity.kt
+++ app/src/main/java/com/example/a2022appjam01team/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.a2022appjam01team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a2022appjam01team.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
